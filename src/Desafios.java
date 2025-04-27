d7a1ab091ccbb7af418601af024a32629e1e2932
Bruno Vasques <brunovasques96@gmail.com>
2025-04-27 19:47:09 -0300

    Initial commit

---
+++ src/Desafios.java
@@ -0,0 +1,13 @@
+public class Media {
+    public static void main(String[] args){
+        double nota1 = 7.0;
+        double nota2 = 8.5;
+        double media = (nota1 + nota2) / 2;
+
+        System.out.println("Média " + media);
+
+        double num1 = 8.6;
+        int numInteiro = (int) num1;
+        System.out.println("Casting de double para int: " + numInteiro);
+    }
+}
