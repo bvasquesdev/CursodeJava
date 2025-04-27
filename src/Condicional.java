750bfd888f167be2a0da4520e8f7c8f003a9a10a
Bruno Vasques <brunovasques96@gmail.com>
2025-04-27 19:49:21 -0300

    aprendendo o básico de java

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Condicional {
+    public static void main(String[] args) {
+
+        int ano = 2021;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        String tipoPlano = "plus";
+
+        if (ano >= 2021){
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir!");
+        }
+
+        if(incluidoNoPlano == true && tipoPlano.equals("plus")){
+            System.out.println("Filme liberado!");
+        } else {
+            System.out.println("Deve pagar a locação.");
+        }
+    }
 }
--- src/Desafios.java
+++ src/Desafios.java
@@ -1,4 +1,4 @@
-public class Media {
+public class Desafios {
     public static void main(String[] args){
         double nota1 = 7.0;
         double nota2 = 8.5;
@@ -9,5 +9,31 @@ public class Media {
         double num1 = 8.6;
         int numInteiro = (int) num1;
         System.out.println("Casting de double para int: " + numInteiro);
+
+        char letra = 'A';
+        String palavra = "MARELO";
+        String mensagem = "A letra é " + letra + " e a palavra é " + palavra;
+        System.out.println(mensagem);
+
+        double precoProduto = 70;
+        int quantidade = 2;
+        double resultado = precoProduto * quantidade;
+        String valor = "O valor total da compra é R$" + resultado;
+        System.out.println(valor);
+
+        double valorEmDolares = 100.5;
+        double taxaDeConversao = 4.94;
+
+        double valorEmReais = valorEmDolares * taxaDeConversao;
+        System.out.println("O valor em reais é: " + valorEmReais);
+
+        double precoOriginal = 50.99;
+        double percentualDesconto = 10.0;
+        double valorDesconto = (percentualDesconto / 100.0) * precoOriginal;
+        double novoPreco = precoOriginal - valorDesconto;
+
+        System.out.println("Preço original: R$" + precoOriginal);
+        System.out.println("Desconto: R$" + valorDesconto);
+        System.out.println("Novo preço com desconto R$:" + novoPreco);
     }
 }
